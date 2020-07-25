ab8299f63567f37627a7883f1a522011773c32e1
Xiaoguang Chen <hbwhcxg@gmail.com>
2020-07-25 01:51:01 -0400

    added addItem and removeItem

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from "reselect";
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -20,8 +21,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToPorps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToPorps = createStructuredSelector ({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(

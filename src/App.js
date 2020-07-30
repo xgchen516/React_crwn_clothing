cba8b524f7611839321f950dacaa4a0108291fff
Xiaoguang Chen <hbwhcxg@gmail.com>
2020-07-30 01:11:00 -0400

    put data on Firebase instead of local project

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector ({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
